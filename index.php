9a47a0e5ccbe22977d2028413965f77020ddb6de
tysonggraham <tyson.g.graham@gmail.com>
2016-01-14 12:06:07 -0700

    hey

--- index.php
+++ index.php
@@ -1,13 +1,71 @@
-<?php 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	<script src="//code.jquery.com/jquery-2.2.0.min.js"></script>
+  <title>Kevin Andres - CS 313</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
+<body>
 
-<body background="smiley.gif">
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+    	<div class="navbar-header">
+      		<a class="navbar-brand" href="#">Kevin Andres - CS 313</a>
+    	</div>
+		<ul class="nav navbar-nav">
+  			<li class="active"><a href="#">Home</a></li>
+  			<li><a href="#">Assignments</a></li>
+		</ul>
+	</div>
+</nav>
+  
+<div class="container">
+	<h3>Kevin Andres - CS 313 Homepage</h3>
+
+	<hr class="featurette-divider">
+
+    <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+            <h2 class="featurette-heading">About me.</h2>
+            <p class="lead">Computer Engineer</p>
+            <ul>
+            	<li>Embedded Systems</li>
+            	<li>Network Architecture</li>
+            	<li>C++ Enthusiast</li>
+            	<li>CSS Hater</li>
+            </ul>
+
+            <p class="lead">Experience</p>
+            <ul>
+            	<li>Cirris Systems - Software Engineer (Starting May 2015)</li>
+            	<li>Object Oriented Programming Tutor (2015-current)</li>
+            	<li>Hughes Network Systems - Software Engineering Intern - Apr 2015-Aug 2015</li>
+            </ul>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+            <a href="/snake.html"><img class="featurette-image img-fluid center-block" src="smiley.jpg" height="35%" width="35%" alt="Generic placeholder image"></a>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
-?>
+
+
+<!-- <!DOCTYPE html>
+<html>
+	<head>
+		<title>My first PHP page</title>
+	</head>
+	<body>
+		<div>
+			This is a pretty awesome page, bossman.
+
+
+
+		</div>
+	</body>
+</html> -->
