78d66e1a4d33508529698c64146b0a233f63ce37
tysonggraham <tyson.g.graham@gmail.com>
2016-01-14 12:16:19 -0700

    hey

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <title>Kevin Andres - CS 313</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel='stylesheet' href="styles.css">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
