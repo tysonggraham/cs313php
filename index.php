546ca08a594246aaf2e71471f2f103eb66bebe5e
tysonggraham <tyson.g.graham@gmail.com>
2016-01-14 12:12:33 -0700

    hey

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
-<body style="background-image:url(smiley.gif)">
+<body style="background-image:url(http://www.smiley-faces.org/wallpaper/smiley-face-wallpaper-014.jpg)">
 
 <nav class="navbar navbar-inverse">
 	<div class="container-fluid">
