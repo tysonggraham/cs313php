3bdc6250b4eda75bdd63f80e5d383877e0fb258f
tysonggraham <tyson.g.graham@gmail.com>
2016-01-19 09:08:27 -0700

    work

--- index.php
+++ index.php
@@ -5,18 +5,7 @@
   <title>Tyson Graham - CS 313</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel='stylesheet' href="styles.css">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  <script>
-    var Clicks = 0 ;
-    function AddClick(animal){
-      if()
-    Clicks = Clicks + 1;
-    document.getElementById('CountedClicks').innerHTML =  Clicks ;
-    }
-  </script>
+
 
 </head>
 
