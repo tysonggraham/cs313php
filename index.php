306f26f036d6e8f21b63d7423c14df4ef5a7ce21
tysonggraham <tyson.g.graham@gmail.com>
2016-01-19 09:09:28 -0700

    work

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
   <title>Tyson Graham - CS 313</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
 </head>
 
 <body>
