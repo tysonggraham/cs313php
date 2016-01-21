0929a8907b3f899b759085f104c247ec764ecf71
tysonggraham <tyson.g.graham@gmail.com>
2016-01-20 21:25:18 -0700

    fixed permissions

--- index.php
+++ index.php
@@ -34,28 +34,32 @@
     		<h3>Tyson Graham - CS 313 Homepage</h3>
 
 	    	<hr class="featurette-divider">
+			<img class="featurette-image img-fluid center-block" src="raccoon.jpg" alt="Generic placeholder image"></img>
 
-			<div class="col-md-5 col-md-pull-7">
-				<img class="featurette-image img-fluid center-block" src="wedding.jpg" height="35%" width="35%" alt="Generic placeholder image"></img>
-			</div>
+	        <div class="row featurette">
+				<div class="col-md-5 col-md-pull-7">
+					<img class="featurette-image img-fluid center-block" src="raccoon.jpg" alt="Generic placeholder image"></img>
+				</div>
+	      	</div>
 	        <div class="col-md-7 col-md-push-5">
-	              <h2 class="featurette-heading">About me.</h2>
-	              <p class="lead">Computer Science Major</p>
-	              <ul>
-	              	<li>Node Developer</li>
-	                <li>SPA Enthusiest</li>
-	                <li>Happily Married Future Father</li>
-	              	<li>Lifelong Student</li>
-	              	<li>Discrete Math Lover</li>
-	              </ul>
+				<img class="featurette-image img-fluid center-block" src="raccoon.jpg" alt="Generic placeholder image"></img>
+				<h2 class="featurette-heading">About me.</h2>
+				<p class="lead">Computer Science Major</p>
+				<ul>
+					<li>Node Developer</li>
+					<li>SPA Enthusiest</li>
+					<li>Happily Married Future Father</li>
+					<li>Lifelong Student</li>
+					<li>Discrete Math Lover</li>
+				</ul>
 
-	              <p class="lead">Interests</p>
-	              <ul>
-	              	<li>Web Development</li>
-	              	<li><a href="https://nodejs.org/en/">Node Js</a></li>
-	                <li><a href="https://laravel.com/">Laravel</a></li>
-	              	<li><a href="https://www.youtube.com/watch?v=MkWTVwJEQ7c">Animal Battles(e.g. snake vs squirrel, shark vs shark)</a></li>
-	              </ul>
+				<p class="lead">Interests</p>
+				<ul>
+					<li>Web Development</li>
+					<li><a href="https://nodejs.org/en/">Node Js</a></li>
+					<li><a href="https://laravel.com/">Laravel</a></li>
+					<li><a href="https://www.youtube.com/watch?v=MkWTVwJEQ7c">Animal Battles(e.g. snake vs squirrel, shark vs shark)</a></li>
+				</ul>
 	        </div>
 		</div>
 	</div>
