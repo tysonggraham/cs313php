f1dd91d32dcb73ec6cc040847721b1fb1b930957
tysonggraham <tyson.g.graham>
2016-01-30 14:28:22 -0700

    does it work?

--- week03/resultsphpheader.php
+++ week03/resultsphpheader.php
@@ -7,64 +7,66 @@ $content = file_get_contents("./$resultsfile", true);
 
 //Build my results arrays
 
-$meats = array(
-	"Carne_Asada" => substr_count($content, "Carne Asada"),
-	"Chicken" => substr_count($content, "Chicken"),
-	"Carnitas" => substr_count($content, "Carnitas"),
-	"Ground_Beef" => substr_count($content, "Ground Beef"),
-	"Barbacoa" => substr_count($content, "Barbacoa"),
-	"Fish" => substr_count($content, "Fish"),
-	"Vegetarian" => substr_count($content, "Vegetarian")
+$redeemingquality = array(
+	"Good_Kisser" => substr_count($content, "Good Kisser"),
+	"Good_Cook" => substr_count($content, "Good Cook"),
+	"Shares_My_Interests" => substr_count($content, "Shares My Interests"),
+	"Skilled_in_Combat" => substr_count($content, "Skilled in Combat Beef"),
+	"Good_Provider" => substr_count($content, "Good Provider"),
+	"Loyal" => substr_count($content, "Loyal"),
+	"Highly_Intelligent" => substr_count($content, "Highly Intelligent"),
+	"Cute_Bum" => substr_count($content, "Cute Bum")
 );
 
 // print_r($meats);
 
-$rices = array(
-	"Cilantro_Rice" => substr_count($content, "Cilantro Rice"),
-	"Spanish_Rice" => substr_count($content, "Spanish Rice"),
-	"White_Rice" => substr_count($content, "White Rice"),
-	"Brown_Rice" => substr_count($content, "Brown Rice"),
-	"No_Rice" => substr_count($content, "No Rice")
+$perfectdate = array(
+	"Going_Hunting" => substr_count($content, "Going Hunting"),
+	"Teasing_the_Weak" => substr_count($content, "Teasing the Weak"),
+	"Volunteering_for_the_poor" => substr_count($content, "Volunteering for the poor"),
+	"Going_Dancing" => substr_count($content, "Going Dancing"),
+	"Watching_the_sun_go_down" => substr_count($content, "Watching the sun go down")
 );
 
 // print_r($rices);
 
 //This may cause issues with the currently existing beans variable
-$beans = array(
-	"Refried_Beans" => substr_count($content, "Refried Beans"),
-	"Black_Beans" => substr_count($content, "Black Beans"),
-	"Pinto_Beans" => substr_count($content, "Pinto Beans"),
-	"No_Beans" => substr_count($content, "No Beans")
+$eat = array(
+	"Pizza" => substr_count($content, "Pizza"),
+	"Sugar_Cubes" => substr_count($content, "Sugar Cubes"),
+	"Anything_really" => substr_count($content, "Anything really"),
+	"Grubs" => substr_count($content, "Grubs"),
+	"Elephant_Seal" => substr_count($content, "Elephant Seal")
 );
 
 // print_r($beans);
 
-$mediums = array(
-	"Thick_Tortilla" => substr_count($content, "Thick Tortilla"),
-	"Thin_Tortilla" => substr_count($content, "Thin Tortilla"),
-	"Salad" => substr_count($content, "Salad")	
+$lifespan = array(
+	"1_to_5" => substr_count($content, "1 to 5"),
+	"10_to_20" => substr_count($content, "10 to 20"),
+	"30_to_100" => substr_count($content, "30 to 100"),
+	"None" => substr_count($content, "None")	
 );
 
 // print_r($beans);
 
 //This may cause issues with the currently existing beans variable
-$tomato_stuff = array(
-	"Plain" => substr_count($content, "Plain"),
-	"Pico_de_Gallo" => substr_count($content, "Pico de Gallo"),
-	"Salsa" => substr_count($content, "Salsa"),
-	"None" => substr($content, "None")	
+$provide = array(
+	"Food" => substr_count($content, "Food"),
+	"Love" => substr_count($content, "Love"),
+	"Warmth" => substr_count($content, "Warmth"),
+	"Riches" => substr_count($content, "Riches"),
+	"Protection" => substr($content, "Protection")	
 );
 
 // print_r($tomato_stuff);
 
-$fixings = array(
-	"Lettuce" => substr_count($content, "Lettuce"),
-	"Sour_Cream" => substr_count($content, "Sour Cream"),
-	"Guacamole" => substr_count($content, "Guacamole"),
-	"Cilantro" => substr_count($content, "Lettuce"),
-	"Lime" => substr_count($content, "Lime"),
-	"Jalapenos" => substr_count($content, "Jalapenos"),
-	"Onions" => substr_count($content, "Onions")
+$quality = array(
+	"Romantic" => substr_count($content, "Romantic"),
+	"Service" => substr_count($content, "Service"),
+	"Purse" => substr_count($content, "Purse"),
+	"Protect" => substr_count($content, "Protect"),
+	"Cute" => substr_count($content, "Cute")
 );
 // print_r($fixings);
 
