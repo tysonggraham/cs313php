935c5334318aad7198fdfb716a2b49859cca2f57
tysonggraham <tyson.g.graham>
2016-01-30 14:44:31 -0700

    does it work?

--- index.php
+++ index.php
@@ -1,71 +1,65 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <title>Kevin Andres - CS 313</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<title>Tyyyyyyyyyyyyyyyyyyyyyyyyy Graham - CS 313</title>
+	<link rel='stylesheet' href="styles.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script>
+		var Clicks = 0 ;
+		function AddClick(animal){
+			  if() {
+			Clicks = Clicks + 1;
+			document.getElementById('CountedClicks').innerHTML =  Clicks ;
+		}
+	</script>
 </head>
-<body>
+<body class="grad1">
+	<div class="page" id="main_page">
+	    <nav class="navbar navbar-inverse">
+	    	<div class="container-fluid">
+	        	<div class="navbar-header">
+	          		<a class="navbar-brand" href="#">Tyson Graham - CS 313</a>
+	        	</div>
+	    		<ul class="nav navbar-nav">
+	      			<li class="active"><a href="#">Home</a></li>
+	      			<li class="active"><a href="assignments.html">Assignments</a></li>
+	      			<li><a href="spiritanimal.html">Guess My Spirit Animal</a></li>
+	    		</ul>
+	    	</div>
+	    </nav>
+	  
+	    <div class="container">
+    		<h3>Tyson Graham - CS 313 Homepage</h3>
 
-<nav class="navbar navbar-inverse">
-	<div class="container-fluid">
-    	<div class="navbar-header">
-      		<a class="navbar-brand" href="index.php">Kevin Andres - CS 313</a>
-    	</div>
-		<ul class="nav navbar-nav">
-  			<li class="active"><a href="index.php">Home</a></li>
-  			<li><a href="assignments.php">Assignments</a></li>
-		</ul>
+	    	<hr class="featurette-divider">
+
+	        <div class="row featurette">
+				<div class="col-md-7">
+					<img class="picture featurette-image img-fluid center-block" height="360px" width="550px" src="wedding.jpg" alt="Generic placeholder image"></img>
+				</div>
+		        <div class="col-md-5">
+					<h2 class="featurette-heading">About me.</h2>
+					<p class="lead">Computer Science Major</p>
+					<ul>
+						<li>Node Developer</li>
+						<li>SPA Enthusiast</li>
+						<li>Happily Married Future Father</li>
+						<li>Lifelong Student</li>
+						<li>Discrete Math Lover</li>
+					</ul>
+
+					<p class="lead">Interests</p>
+					<ul>
+						<li>Web Development</li>
+						<li><a href="https://nodejs.org/en/">Node Js</a></li>
+						<li><a href="https://laravel.com/">Laravel</a></li>
+						<li><a href="https://www.youtube.com/watch?v=MkWTVwJEQ7c">Animal Battles(e.g. snake vs squirrel, shark vs shark)</a></li>
+					</ul>
+		        </div>
+	      	</div>
+		</div>
 	</div>
-</nav>
-  
-<div class="container">
-	<h3>Kevin Andres - CS 313 Homepage</h3>
-
-	<hr class="featurette-divider">
-
-    <div class="row featurette">
-        <div class="col-md-7 col-md-push-5">
-            <h2 class="featurette-heading">About me.</h2>
-            <p class="lead">Computer Engineer</p>
-            <ul>
-            	<li>Embedded Systems</li>
-            	<li>Network Architecture</li>
-            	<li>C++ Enthusiast</li>
-            	<li>CSS Hater</li>
-            </ul>
-
-            <p class="lead">Experience</p>
-            <ul>
-            	<li>Cirris Systems - Software Engineer (Starting May 2016)</li>
-            	<li>Object Oriented Programming Tutor (2015-current)</li>
-            	<li>Hughes Network Systems - Software Engineering Intern - Apr 2015-Aug 2015</li>
-            </ul>
-        </div>
-        <div class="col-md-5 col-md-pull-7">
-            <a href="/snake.html"><img class="featurette-image img-fluid center-block" src="me.jpg" height="35%" width="35%" alt="Generic placeholder image"></a>
-        </div>
-    </div>
-</div>
-
 </body>
 </html>
-
-
-<!-- <!DOCTYPE html>
-<html>
-	<head>
-		<title>My first PHP page</title>
-	</head>
-	<body>
-		<div>
-			This is a pretty awesome page, bossman.
-
-
-
-		</div>
-	</body>
-</html> -->
