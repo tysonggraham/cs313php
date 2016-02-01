b032d5b467011c22d9410acbc8cff980e89add42
tysonggraham <tyson.g.graham>
2016-02-01 12:31:12 -0700

    changed link to external site for spirit animal quiz

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tyyyyyyyyyyyyyyyyyyyyyyyyy Graham - CS 313</title>
+	<title>Tyson Graham - CS 313</title>
 	<link rel='stylesheet' href="styles.css">
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-	<script>
-		var Clicks = 0 ;
-		function AddClick(animal){
-			  if() {
-			Clicks = Clicks + 1;
-			document.getElementById('CountedClicks').innerHTML =  Clicks ;
-		}
-	</script>
 </head>
 <body class="grad1">
 	<div class="page" id="main_page">
