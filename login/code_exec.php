fbbcb5dae2cca106dea65387d56fac782b15fa7c
tysonggraham <tyson.g.graham>
2016-02-22 09:59:25 -0700

    should be pulling all users successfully now

--- login/code_exec.php
+++ login/code_exec.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-include('connection.php');
+include('openShiftConnection.php');
+$db = loadDatabase();
 $fname=$_POST['fname'];
 $lname=$_POST['lname'];
 $password=$_POST['password'];
@@ -11,7 +12,8 @@ $is_human=$_POST['is_human'];
 $vote_cnt=$_POST['vote_cnt'];
 $address=$_POST['address'];
 // $pic=$_POST['pic'];
-mysql_query("INSERT INTO users(first, last, password, phone, email, job_name, is_human, vote_cnt, address)VALUES('$fname', '$lname', '$password', '$phone', '$email', '$job_name', '$is_human', '$vote_cnt', '$address')");
+$sql = "INSERT INTO users(first, last, password, phone, email, job_name, is_human, vote_cnt, address)VALUES('$fname', '$lname', '$password', '$phone', '$email', '$job_name', '$is_human', '$vote_cnt', '$address')";
+$query = $db->query($sql);
 header("location: test.php?remarks=success");
 // mysql_close($con);
 ?>
