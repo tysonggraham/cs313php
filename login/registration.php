9a690fdbd6d781bd26f63da2c57711e4261d130a
tysonggraham <tyson.g.graham>
2016-02-23 00:03:28 -0700

    added image upload to registration page.

--- login/registration.php
+++ login/registration.php
@@ -1,5 +1,9 @@
 <html>
 <head>
+  <link rel='stylesheet' href="styles.css">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="registrationStyles.css">
   <script type="text/javascript">
     document.getElementById("uploadBtn").onchange = function () {
