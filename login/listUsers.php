b2fdd449dc79ca685279f4bd829df1cdf7eb29d3
tysonggraham <tyson.g.graham>
2016-02-23 09:21:12 -0700

    added image upload to registration page.

--- login/listUsers.php
+++ login/listUsers.php
@@ -8,7 +8,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <style>
-html { 
+div.container { 
 background-attachment: fixed;
 background: url(http://i.imgur.com/DQ59KbW.jpg) no-repeat center center fixed; 
     -o-background-size: cover;
