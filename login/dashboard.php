3ce626b050779f49b864977fafd4cabe3c923e5c
tysonggraham <tyson.g.graham>
2016-02-25 09:49:21 -0700

    trying to fix the map api calls

--- login/dashboard.php
+++ login/dashboard.php
@@ -82,6 +82,9 @@ else
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 <meta charset="utf-8">
 <title>Companion Finder Home</title>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <style>
 html, body {
 	height: 100%;
